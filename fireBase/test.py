fc07402dda6e7e0cc10b052ab66d19ba9d9cb11d
whalehippo <dany950319@gmail.com>
2017-12-03 13:04:09 +0900

    test

--- fireBase/test.py
+++ fireBase/test.py
@@ -2,7 +2,9 @@ import firebaseServer as fs
 
 firebase = fs.firebaseServer(8765)
 
+url = raw_input("input callback url : ");
+firebase.callback(url);
 while True:
     url = raw_input("input url : ");
-    val = raw_input("input url : ");
-    firebase.callback(url);
+    val = raw_input("input val : ");
+    firebase.update(url, val);
